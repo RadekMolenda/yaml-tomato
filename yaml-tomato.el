7d835694d25cc4b59b6040d303f192ec824a54d7
Radek Molenda <rmolenda@leadformance.com>
2015-11-22 20:59:25 +0100

    Add package metadata

--- yaml-tomato.el
+++ yaml-tomato.el
@@ -1,15 +1,36 @@
+;;; yaml-tomato.el --- Some useful functions for editing yaml files
+
+;; Author: qrczeno
+;; Created: 22-Nov-2015
+;; Version: 0.1
+;; Keywords: Yaml
+;; Package-Requires: ((s "1.9"))
+
+;;; Commentary:
+
+;; Installation:
+;;   - Put `yaml-tomato.el' in your Emacs load-path.
+;;   - Add this line to your .emacs file:
+;;       (require 'yaml-tomato)
+
+;;; Code:
+
+;;;###autoload
 (require 's)
 
 (defvar spaces-per-tab 1)
 
 (defun count-white-spaces (string)
+  "Count how many white spaces there are at the beginning of the STRING."
   (s-count-matches " " (car (s-slice-at "[:alnum]"
                                         string))))
 
 (defun get-yaml-key (string)
+  "Get the yaml tag from STRING."
   (car (s-slice-at ":" (s-trim string))))
 
 (defun yaml-current-path ()
+  "Get the tags path under cursor."
   (let* ((path '())
          (get-key (lambda () (get-yaml-key (s-trim (thing-at-point 'line t)))))
          (search-previous (lambda (spaces) (re-search-backward (s-concat "^" (s-repeat spaces " ") "[a-zA-Z\(]") nil t nil)))
@@ -24,13 +45,14 @@
     path))
 
 (defun yaml-tomato/show-current-path ()
-  "show current yaml path in message buffer"
+  "Show current yaml path in message buffer."
   (interactive)
   (message (s-join "." (yaml-current-path))))
 
 (defun yaml-tomato/copy ()
-  "copy current path to kill-ring"
+  "Copy current path to 'kill-ring'."
   (interactive)
   (kill-new (s-join "." (yaml-current-path))))
 
 (provide 'yaml-tomato)
+;;; yaml-tomato.el ends here
